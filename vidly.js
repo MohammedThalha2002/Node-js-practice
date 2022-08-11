9768007e6838acfde8d2742aa5723ffc168c00e7
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2022-08-11 22:53:02 +0530

    joi validation

--- vidly.js
+++ vidly.js
@@ -18,16 +18,26 @@ app.get("/api/genere", (req, res) => {
 
 app.get("/api/genere/:id", (req, res) => {
     const gene = genere.find(g => g.id == req.params.id)
-    if(!gene) res.status(404).send("No genere found with the ID")
+    if (!gene) res.status(404).send("No genere found with the ID")
     res.send(gene)
 })
 
-app.post("/api/genere")
+app.post("/api/genere", (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    const { error } = schema.validate(req.body)
+    if (error) res.status(404).send(error.message)
+    genere.push({
+        id: genere.length + 1,
+        name: req.body.name
+    })
+})
 
 
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`Listening to the port ${PORT}`)
 })
